cc03be2ddca924b82ba88402a81d12272b51567d
Pedro Caetano <pedro.kaetano2003@gmail.com>
2023-11-21 18:59:59 -0300

    Arrumando a funcionalidade de calcular a data  até o evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,12 @@ AOS.init();
 
 const dataDoEvento = new Date("apr 10, 2024 23:59:59");
 const timeStampDoEvento = dataDoEvento.getTime();
-// const diaAniversario = new Date (document.getElementById("data_aniversario").value);
 
 const contaHoras = setInterval(function() {
     const agora = new Date();
     const timeStampAtual = agora.getTime();
 
     const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
-    // const distanciaAteAniversario = diaAniversario - timeStampAtual;
 
     const diasEmMs = 1000 * 60 * 60 * 24;
     const horasEmMs = 1000 * 60 * 60;
@@ -21,16 +19,48 @@ const contaHoras = setInterval(function() {
     const segundosAteEvento = Math.floor((distanciaAteEvento % minutosEmMs) / 1000);
 
     document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
-    // document.getElementById('data_aniversario').innerHTML = `${diasAteEvento}d ${horasAteEvento}h`;
 
     if (distanciaAteEvento < 0) {
         clearInterval (contaHoras);
         document.getElementById('contador').innerHTML = 'Evento Expirado'
     }
+}, 1000);
 
-    // if (distanciaAteAniversario >= 90) {
-    //     .innerHTML = `${diasRestantes}d ${horasRestantes}h`;
-    // } else {
-    //     .innerHTML = "O tempo restante é menor que 3 meses";
-    // }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const confirmButton = document.querySelector(".button-calcular");
+    const dateInput = document.getElementById("dia");
+    const spanMessage = document.querySelector(".data_aniversario");
+
+
+    confirmButton.addEventListener("click", function (event) {
+        // Impede o envio do formulário
+        event.preventDefault();
+
+
+        const selectedDate = new Date(dateInput.value);
+        const currentDate = new Date();
+
+
+        // Calcula a diferença em milissegundos
+        const timeDifference = selectedDate - currentDate;
+
+
+        // Converte a diferença para meses
+        const monthsDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30.44));
+
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+        // const daysDifference = timeDifference / (1000 * 60 * 60 * 24 / (30.44));
+
+
+        // Verifica se a diferença é inferior a 3 meses
+        if (monthsDifference < 3 & days >= 2 ) {
+            spanMessage.textContent = `Faltam: ${days}d. A data é menor que 3 meses!`;
+        } else if (days < 1) {
+            spanMessage.textContent = `É necessário que a data tenha mais que 3 meses!`;
+        } else {
+            spanMessage.textContent = `Faltam: ${monthsDifference}m. É possível contratar o serviço!`;
+        }
+    });
 }, 1000);
